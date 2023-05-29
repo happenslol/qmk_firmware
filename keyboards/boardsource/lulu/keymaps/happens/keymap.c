084a61f9d67d4dcfb65e33242a699600fc4b3678
Hilmar Wiegand <me@hwgnd.de>
2023-05-29 14:34:55 +0200

    Update lulu keymap config

--- keyboards/boardsource/lulu/keymaps/happens/keymap.c
+++ keyboards/boardsource/lulu/keymaps/happens/keymap.c
@@ -31,7 +31,7 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT(
-  KC_ESC,   KC_HOME, KC_END,  KC_INS,  KC_PGDN, KC_PGUP,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DEL,  KC_BSPC,
+  KC_ESC,   KC_HOME, KC_END,  KC_INS,  KC_PGDN, KC_PGUP,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,  KC_BSPC,
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RALT,
   KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,    KC_PSCR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = LAYOUT(
-  KC_TILD,  _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  KC_GRV,   _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   _______,  KC_1,    KC_2,    KC_3,    KC_0,    _______,                     _______, _______, _______, _______, _______, _______,
   _______,  KC_4,    KC_5,    KC_6,    _______, _______,                     _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
   _______,  KC_7,    KC_8,    KC_9,    _______, _______, KC_MUTE,   _______, _______, _______, _______, _______, _______, _______,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_RAISE] = LAYOUT(
-  KC_GRV,   _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, KC_DEL,
+  KC_TILD,  _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, KC_DEL,
   _______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   _______,  _______, _______, _______, _______, _______,                     _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
   _______,  _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
