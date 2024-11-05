314af04ffa7b08379da3596a2a5e30c551850028
Hilmar Wiegand <me@hwgnd.de>
2024-11-05 22:36:16 +0100

    Update form-zero keymap

--- keyboards/form_zero/keymaps/default/keymap.c
+++ keyboards/form_zero/keymaps/default/keymap.c
@@ -29,8 +29,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_ortho_5x12(
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_ENT,   _______,
         _______,  KC_P1,    KC_P2,    KC_P3,    KC_P0,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  KC_P4,    KC_P5,    KC_P6,    _______,  _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
-        _______,  KC_P7,    KC_P8,    KC_P9,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  KC_P4,    KC_P5,    KC_P6,    KC_F5,    _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
+        _______,  KC_P7,    KC_P8,    KC_P9,    KC_F6,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
     [_RAISE] = LAYOUT_ortho_5x12(
