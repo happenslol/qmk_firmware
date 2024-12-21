5fe32fcee1bd0df9546ae5396d5c33961be5c52a
Hilmar Wiegand <me@hwgnd.de>
2024-12-21 02:53:23 +0100

    Update control keymap

--- keyboards/happy/control/keymaps/default/keymap.c
+++ keyboards/happy/control/keymaps/default/keymap.c
@@ -18,19 +18,18 @@
 #define EXT1 MO(_EXT1)
 #define EXT2 MO(_EXT2)
 
-#define CST0 MT(MOD_RSFT, KC_ENT)
-#define CST1 LT(_FUNC, KC_LEFT)
-#define CST2 LT(_MENU, KC_DOWN)
-#define CST3 MT(MOD_RGUI, KC_UP)
-#define CST4 MT(MOD_RCTL, KC_RGHT)
+#define CST0 LT(_FUNC, KC_LEFT)
+#define CST1 LT(_MENU, KC_DOWN)
+#define CST2 MT(MOD_RGUI, KC_UP)
+#define CST3 MT(MOD_RCTL, KC_RGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_5x12(
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_RALT,
         EXT1,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  CST0,
-        KC_LCTL,  KC_LGUI,  KC_LALT,  EXT2,     LOWER,    KC_SPC,   NAV,      RAISE,    CST1,     CST2,     CST3,     CST4
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,
+        KC_LCTL,  KC_LGUI,  KC_LALT,  EXT2,     LOWER,    KC_SPC,   NAV,      RAISE,    CST0,     CST1,     CST2,     CST3
     ),
     [_LOWER] = LAYOUT_ortho_5x12(
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
