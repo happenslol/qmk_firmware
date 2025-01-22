c2d83a3bdfe1d903cda2f8e55dd02ac9a09cb115
Hilmar Wiegand <me@hwgnd.de>
2025-01-22 19:54:38 +0100

    Update keymaps

--- keyboards/happy/control/config.h
+++ keyboards/happy/control/config.h
@@ -1,23 +1,8 @@
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
 #define PERMISSIVE_HOLD
 #define USB_SUSPEND_WAKEUP_DELAY 200
 
 #define TAPPING_TERM 100
+#define RETRO_TAPPING
 #define HOLD_ON_OTHER_KEY_PRESS
